0e330ecc9e87b27fcc2843ae73041a968ec8ecc2
Felipe Brigagão de Almeida <felipe-almeida2012@hotail.com>
2022-07-20 14:55:17 -0300

    Added SwiftLint to the project

--- AudioDB/ContentView.swift
+++ AudioDB/ContentView.swift
@@ -8,11 +8,14 @@
 import SwiftUI
 
 struct ContentView: View {
+  let names = ["Felipe", "Gustavo", "Luiz", "Iorgers"]
+  let colonOnWrongSide: Int = 0
   var body: some View {
     VStack {
-      Text("Hello, Integration!")
-        .padding()
-      
+      ForEach(names, id: \.self) { name in
+        Text(name)
+          .padding()
+      }
       Text("Hello, Group 1!")
         .padding()
     }
