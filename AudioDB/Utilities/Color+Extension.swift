1c82b84927cc80961b01cb362046fe9b4b76a3a0
LuizAraujo2020 <luizcarlos_bsb2006@hotmail.com>
2022-07-21 15:15:51 -0300

    chore: created Color+Extension and added some colors from de Xcasset

---
+++ AudioDB/Utilities/Color+Extension.swift
@@ -0,0 +1,20 @@
+//
+//  Color+Extension.swift
+//  AudioDB
+//
+//  Created by Luiz Araujo on 21/07/22.
+//
+
+import SwiftUI
+
+extension Color {
+    public static var primaryColor: UIColor {
+        return UIColor(named: "primaryColor")!
+    }
+    public static var backgroundColor: UIColor {
+        return UIColor(named: "backgroundColor")!
+    }
+    public static var textColor: UIColor {
+        return UIColor(named: "textColor")!
+    }
+}
