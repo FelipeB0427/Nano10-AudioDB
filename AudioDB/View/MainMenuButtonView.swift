29c0ca9356d2818e75bbce3924095b585ec66aaa
LuizAraujo2020 <luizcarlos_bsb2006@hotmail.com>
2022-07-21 14:48:01 -0300

    added the crazy file

---
+++ AudioDB/View/MainMenuButtonView.swift
@@ -0,0 +1,20 @@
+//
+//  MainMenuButtonView.swift
+//  AudioDB
+//
+//  Created by Luiz Araujo on 20/07/22.
+//
+
+import SwiftUI
+
+struct MainMenuButtonView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct MainMenuButtonView_Previews: PreviewProvider {
+    static var previews: some View {
+        MainMenuButtonView()
+    }
+}
